995b6c0f6de301f4f1c46f74103d5638a393fc62
Rafael Marques <rafael.fmarques@outlook.com>
2021-09-26 16:35:21 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import fs from 'fs';
+import { MatchReader } from './composition/MatchReader';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './composition/CsvFileReader';
 
-const matches: string[][] = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const csvFileReader = new CsvFileReader('football.csv');
+
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+let manUnitedWins = 0;
+
+for (const match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
